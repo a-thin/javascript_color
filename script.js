a551e4bf1d0c2ab1cfadc5921410f7adcc577692
あーてぃん <131327071+a-thin@users.noreply.github.com>
2024-09-24 09:59:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 // JavaScript
-console.log('Hello world!');
-console.log(document.getElementById('myColor').value);
+
+//定数の宣言
+const color = document.getElementById('myColor');
+const text = document.getElementById('bodyText');
+
+//console.log(document.getElementById('myColor').value);
+//document.getElementById('bodyText').textContent = '文字の置き換え❤️';
+//document.getElementById('myColor').value;
+
+text.textContent = color.value;
+
+// カラーピッカーで色が選択されたら、changeColorを動作させる
+color.addEventListener('input', changeColor);
+
+function changeColor(){
+    // カラーコードを表示
+      text.textContent = color.value;
+    }
+    
+    // カラーピッカーで色が選択されたらchangeColorを動作させる
+    color.addEventListener('input', changeColor);
+
+    function changeColor(){
+        // カラーコードを表示
+          text.textContent = color.value;
+        // 背景色を変更
+          document.body.style.backgroundColor = color.value;
+        }
